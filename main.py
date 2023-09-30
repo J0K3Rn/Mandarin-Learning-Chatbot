0f1e735a155a485f6ff5f77331d7ecf717234c70
J0K3Rn <JosephKeeganRichardson@gmail.com>
2023-09-30 15:33:54 -0500

    Putting everything in functions and organizing

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ import json
 import pandas as pd
 from random import choice
 
-
-prompt = "Generate 3 sentences in Chinese using the character 我."
-
+# Start Setup
 config = dotenv.dotenv_values(".env")
 openai.api_key = config['OPENAI_API_KEY']
 #openai.api_key_path = ".env"
-curr_char = '我'
+#curr_word = '我'
 SAVED_OUTPUT = "data/saved_output.json"
 dictionary = []  # list of dictionaries
+
+# Read csv and convert to dictionary
+try:
+    df = pd.read_csv('data/chinese_words.csv')
+except FileNotFoundError:
+    print("File not found!")
+    exit(1)
+
+to_learn = df.to_dict(orient="records")
+total = 69740745  # Accumulation of all Frequency Count in the CSV <Calculated manually>
+
+# End Setup
 
 
 def reload_dictionary():
     global dictionary
     with open(SAVED_OUTPUT, "r") as file:
         dictionary = json.load(file)
-    #print(type(dictionary))
-    #print(dictionary)
+    # print(type(dictionary))
+    # print(dictionary)
 
 
-def add_character(character):
+def add_word(character):
     global dictionary
     # Be sure to check if it exists first
     new_dict = {
@@ -37,26 +47,27 @@ def add_character(character):
         file.write(json.dumps(dictionary, indent=4, ensure_ascii=False))
 
 
-def check_if_character_exists(character):
+def check_if_word_exists(character):
     global dictionary
     for index in dictionary:
         if index['character'] == character:
             print("Character exists!")
-            return
-    add_character(character)
+            return True
+    return False
+    # add_character(character)
 
 
-def get_character_examples(character):
+def get_examples(character):
     global dictionary
     example_list = []
     for index in dictionary:
-        #print(index)
+        # print(index)
         if index['character'] == character:
             example_list = index['examples']
     return example_list
 
 
-def add_character_example(character, example):
+def add_example(character, example):
     global dictionary
     # Update
     for index, dic in enumerate(dictionary):
@@ -70,64 +81,70 @@ def add_character_example(character, example):
             return
 
 
-def check_if_example_exists(character, example):
+def example_exists(character, example):
     global dictionary
     for index in dictionary:
         if index['character'] == character:
-            #print("Character exists!")
+            # print("Character exists!")
             for ex in index['examples']:
                 if ex == example:
-                    return
-    add_character_example(character, example)
+                    return True
+    return False
+    # add_example(character, example)
 
 
-# Read csv and convert to dictionary
-try:
-    df = pd.read_csv('data/chinese_words.csv')
-except FileNotFoundError:
-    print("File not found!")
-    exit(1)
+def chatgpt_query(word):
+    prompt = f"Generate 3 sentences in Chinese using {word}."
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        prompt=prompt
+        # messages=[{"role": "user", "content": f'Generate 3 sentences in Chinese using the character {word}.'}]
+    )
+    print(response)
+    print(response['choices'][0]['message']['content'])
 
-to_learn = df.to_dict(orient="records")
-total = 69740745  # Accumulation of all Frequency Count in the CSV
-current_entry = choice(to_learn)
-character = current_entry['Chinese']
-meaning = current_entry['English']
-frequency = current_entry['Frequency Count']/total
-frequency = f'{frequency:.9f}%'
+    output_to_parse = response['choices'][0]['message']['content']
+    chatgpt_examples = output_to_parse.split('\n')
 
-print(current_entry)
-print(character)
-print(meaning)
-print(frequency)
+    for example in chatgpt_examples:
+        example = example[3:]
+        print(example)
+        if check_if_word_exists(word):
+            # if so, get examples
+            if example_exists(word, example):
+                add_example(word, example)
+            examples = get_examples(word)
+            print(examples)
+            return examples
+        else:
+            add_word(word)
+            add_example(word, example)
 
 
-reload_dictionary()  # Need this function ran every time the program starts
-#check_if_character_exists("abc")
-# if so, get examples
-examples = get_character_examples("abc")
-print(examples)
-#check_if_example_exists("abc", "Hello World!")
+def app():
+    global to_learn
+    reload_dictionary()  # Need this function ran every time the program starts
 
-#
-# response = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo-0613",
-#     #prompt=prompt
-#     messages=[{"role": "user", "content": f'Generate 3 sentences in Chinese using the character {curr_char}.'}]
-# )
-#
-# print(response)
-#
-# print(response['choices'][0]['message']['content'])
-#
-# output_to_parse = response['choices'][0]['message']['content']
-# examples = output_to_parse.split('\n')
-# for example in examples:
-#     example = example[3:]
-#     # Read csv, check if example already exists, if not then add to list
-#     # sorted by character, and then check if sentence exists
-#     print(example)
-#
-# print("a")
-# print(examples)
+    current_entry = choice(to_learn)
+    character = current_entry['Chinese']
+    meaning = current_entry['English']
+    frequency = current_entry['Frequency Count'] / total
+    frequency = f'{frequency:.9f}%'
 
+    # Testing
+    print(current_entry)
+    print(character)
+    print(meaning)
+    print(frequency)
+
+    chatgpt_query(character)
+
+    # Present examples to user
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
